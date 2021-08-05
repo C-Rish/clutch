8dd166c0071a623ae69c377428933343e870bafe
shirish <shirishkhadgi23@gmail.com>
2021-08-05 09:07:18 +0545

    first commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-const form = document.querySelector("form");
-
-const addEmail = async (e) => {
-  e.preventDefault();
-
-  const doc = {
-    email: form.email.value
-  }
-
-  if (doc.email != ""){
-    await fetch('http://localhost:3000/emails', {
-      method: 'POST',
-      body: JSON.stringify(doc),
-      headers: {'Content-Type':'application/json'}
-    });
-    window.location.replace('/index.html');
-  }
-
-
-}
-
-form.addEventListener('submit', addEmail);
+document.getElementsByClassName('carousel').carousel({
+  interval: 2000
+})
